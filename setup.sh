fdb08489cc46c26d301ae3b0a0b4b922eedc8686
Florian "TheTaktik" Olk <olk.florian@googlemail.com>
2020-07-08 18:27:02 +0200

    Setup script WIP

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ emulate -LR zsh
 
 set -ue
 
+# see https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself/4774063
+SCRIPTDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTPATH=$SCRIPTDIR/"setup.sh"
+
 LAST_FOUND_CMD=""
 find_command()
 {
@@ -29,6 +33,7 @@ setup_zsh_config()
   then
     echo " Found existing zsh config."
     echo " Skipping."
+    return 0
   fi
 
   if ! find_command zsh
@@ -36,6 +41,19 @@ setup_zsh_config()
     echo " Aborting!"
     return 1
   fi
+
+  local ZSHRC_SRC=$SCRIPTDIR/".zshrc"
+  local ZSHENV_SRC=$SCRIPTDIR/".zshenv" 
+
+  if ! [ -f $ZSHRC_SRC -a -f $ZSHENV_SRC ]
+  then
+    echo " Install files not found"
+    echo " Aborting."
+    return 1
+  fi
+
+  ln -sf $ZSHRC_SRC $ZSHRC
+  ln -sf $ZSHENV_SRC $ZSHENV
 }
 
 setup_vim_config()
@@ -47,6 +65,7 @@ setup_vim_config()
   then
     echo " Found existing vim config."
     echo " Skipping."
+    return 0
   fi
 
   if ! find_command nvim
@@ -58,6 +77,17 @@ setup_vim_config()
       return 1
     fi
   fi
+
+  local VIMRC_SRC=$SCRIPTDIR/".vimrc"
+
+  if ! [ -f $VIMRC_SRC ]
+  then
+    echo " Install files not found"
+    echo " Aborting."
+    return 1
+  fi
+
+  ln -sf $VIMRC_SRC $VIMRC
 }
 
 setup_i3_config()
@@ -69,6 +99,7 @@ setup_i3_config()
   then
     echo " Found existing i3 config."
     echo " Skipping."
+    return 0
   fi
 
   if ! find_command i3
@@ -76,13 +107,32 @@ setup_i3_config()
     echo " Aborting!"
     return 1
   fi
+
+  local I3CONFIG_SRC=$SCRIPTDIR/"i3config"
+
+  if ! [ -f $I3CONFIG_SRC ]
+  then
+    echo " Install files not found"
+    echo " Aborting."
+    return 1
+  fi
+
+  ln -sf $I3CONFIG_SRC $I3CONFIG
 }
 
 echo "Starting setup"
 
+if [ ! -f $SCRIPTPATH ]
+then
+  echo "$SCRIPTPATH does not point to this script!"
+  echo "Aborting!"
+  exit 1
+fi
+
 if [ ! -d $HOME ]
 then
   echo "\$HOME does not point to a existing directory"
+  echo "Aborting!"
   exit 1
 fi
 
